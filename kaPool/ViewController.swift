22d557e952e9b71cd1db8ad658fa7a2fbc858c5a
Emily Tan <emilytan@emilys-macbook-pro-3.local>
2018-11-29 14:30:30 -0500

    Initial Commit

---
+++ kaPool/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  kaPool
+//
+//  Created by Emily Tan on 11/29/18.
+//  Copyright © 2018 Emily Tan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
